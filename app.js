7dca0ecd8a9c7d196e30d2e21a7f21fd3fe2e9ad
clau <clau.burrito@gmail.com>
2021-12-22 18:47:15 +0200

    Added new contract sample

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const docx_templater = require("docxtemplater");
 const fs = require("fs");
 const performance = require("perf_hooks").performance;
 
-const content = fs.readFileSync("samples/contract.docx", "binary");
+const json = JSON.parse(fs.readFileSync('data.json'));
+const get_type = () => {
+    if (json.selected_type == 1)
+        return { path: json.type_1.file };
+    else if (json.selected_type == 2)
+        return { path: json.type_2.file };
+
+    return {};
+}
+
+const content = fs.readFileSync(get_type().path, "binary");
 const zip = new piz_zip(content);
 const doc = new docx_templater(zip, { paragraphLoop: true, linebreaks: true });
 
-const data = JSON.parse(fs.readFileSync('data.json'));
-doc.render(data);
+doc.render(json.data);
 
 const buffer = doc.getZip().generate({ type: "nodebuffer" });
-fs.writeFile("bin/output.docx", buffer, (error) => {
+fs.writeFile(`bin/output_type_${json.selected_type}.docx`, buffer, (error) => {
     if (error) throw error;
 
-    console.log(`\n[*] built in ${performance.now().toFixed(0)}ms`);
+    console.log(`\n[*] built 'output_type_${json.selected_type}.docx' in ${performance.now().toFixed(0)}ms`);
 });
